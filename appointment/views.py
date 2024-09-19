1c2370c1ae9d3dd2c14aeb6acec61fd23725592a
Andrew Demchuk <demchuk162@gmail.com>
2024-09-19 21:16:01 +0300

    added sending a letter to the patient's email after creating an appointment with a doctor

--- appointment/views.py
+++ appointment/views.py
@@ -1,4 +1,7 @@
 import rest_framework.generics as generics
+from django.core.mail import send_mail, EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -6,7 +9,6 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from patient.models import Appointment
 from .serializers import CreateAppointmentSerializer, AppointmentSerializer
-
 
 
 class CreateAppointmentView(generics.CreateAPIView):
@@ -19,7 +21,18 @@ class CreateAppointmentView(generics.CreateAPIView):
         if patient.role.role == 'patient':
             if serializer.is_valid(raise_exception=True):
                 # Сохранение данных
-                serializer.save(patient=patient)
+                appointment = serializer.save(patient=patient)
+                subject = f"Appointment created with {appointment.doctor.first_name} {appointment.doctor.last_name}"
+                html_message = render_to_string('appointment_email.html', {
+                    'patient_name': patient.first_name,
+                    'doctor_name': appointment.doctor.first_name,
+                    'date': appointment.date,
+                    'time': appointment.time,
+                })
+                message = strip_tags(html_message)
+                email = EmailMultiAlternatives(subject, message, to=[patient.email])
+                email.send()
+
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
