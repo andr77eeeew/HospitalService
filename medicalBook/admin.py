2120d6585dcf6a3c4951cd6d048c586ee1318a08
Andrew Demchuk <demchuk162@gmail.com>
2024-09-20 23:37:57 +0300

    register new model to admin

---
+++ medicalBook/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from medicalBook.models import MedicalBook
+
+
+@admin.register(MedicalBook)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'patient', 'doctor', 'diagnosis', 'treatment', 'description', 'tests', 'created_at')
+    list_filter = ('patient', 'diagnosis')
