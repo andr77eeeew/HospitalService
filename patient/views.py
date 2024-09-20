9ff4e9d7dcbb8df8d710969a9867d3f6c543d31a
Andrew Demchuk <demchuk162@gmail.com>
2024-09-20 23:40:05 +0300

    added new migrations and refactor views

--- patient/views.py
+++ patient/views.py
@@ -1,18 +1,20 @@
 import rest_framework.generics as generics
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-from patient.models import Appointment
 from patient.serializers import PatientSerializer, PatientRegisterSerializer
 from users.models import User
+
 
 # Create your views here.
 
 class PatientsList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = PatientSerializer
+    permission_classes = (JWTAuthentication,)
+    authentication_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         queryset = User.objects.filer(role__role='patient').related_name('role')
@@ -30,5 +32,3 @@ class PatientRegisterView(generics.CreateAPIView):
             serializer.save()
             return Response({'message': 'User created successfully'}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
