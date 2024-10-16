f0a63145919997f8c54a83a063038a81681d0421
Andrew Demchuk <demchuk162@gmail.com>
2024-10-16 18:56:16 +0300

    added new route for notification consumer

--- chat/routing.py
+++ chat/routing.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from chat import consumers
+from chat import chat_consumers, notification_consumers
 
 websocket_urlpatterns = [
-    path('ws/chat/<str:room_name>/', consumers.ChatConsumer.as_asgi()),
+    path('ws/chat/<str:room_name>/', chat_consumers.ChatConsumer.as_asgi()),
+    path('ws/notification/', notification_consumers.NotificationConsumer.as_asgi()),
 ]
