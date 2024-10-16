6d784e824d7a1bab3caa2b0be32e921e8e368cef
Andrew Demchuk <demchuk162@gmail.com>
2024-10-16 18:53:30 +0300

    added new field to ChatRoom and ChatHistory model

--- chat/models.py
+++ chat/models.py
@@ -10,6 +10,7 @@ class ChatRoom(models.Model):
     doctor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='chat_doctor')
     patient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='chat_patient')
     created_at = models.DateTimeField(auto_now_add=True)
+    active_users = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return f'Room between {self.doctor} and {self.patient}'
@@ -24,6 +25,7 @@ class ChatHistory(models.Model):
     is_edited = models.BooleanField(default=False)
     is_deleted = models.BooleanField(default=False)
     replied_to = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='replies')
+    read_status = models.BooleanField(default=False)
 
     def __str__(self):
         if self.is_deleted:
