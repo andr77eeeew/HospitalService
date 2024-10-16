ecc1229a5114e30db805306b9a45645945b248e4
Andrew Demchuk <demchuk162@gmail.com>
2024-10-16 18:57:15 +0300

    added new view and serializer for recent chats

--- chat/views.py
+++ chat/views.py
@@ -3,9 +3,11 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.generics import GenericAPIView
+from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from .models import ChatRoom
+from .serializers import ChatRoomSerializer
 
 
 class ChatRoomView(GenericAPIView):
@@ -13,7 +15,6 @@ class ChatRoomView(GenericAPIView):
     authentication_classes = [JWTAuthentication]
 
     def post(self, request):
-
         if request.user.role.role == 'doctor':
             doctor = request.user.id
             patient = request.data.get('user_id')
@@ -32,3 +33,21 @@ class ChatRoomView(GenericAPIView):
             room = ChatRoom.objects.create(name=room_name, doctor_id=doctor, patient_id=patient)
 
             return Response({'room_name': room.name}, status=status.HTTP_201_CREATED)
+
+
+class RecentlyChat(APIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+    def get(self, request):
+        user = request.user
+
+        if user.role.role == 'doctor':
+            queryset = ChatRoom.objects.filter(doctor=user)
+        elif user.role.role == 'patient':
+            queryset = ChatRoom.objects.filter(patient=user)
+        else:
+            queryset = []
+
+        serializer = ChatRoomSerializer(queryset, many=True, context={'request_user': user, 'request': request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
