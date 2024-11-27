005a78770748614c7d00c9a82b57c3a1cf0f24bc
Andrew Demchuk <demchuk162@gmail.com>
2024-11-27 13:06:58 +0200

    delete logging

--- chat/middleware.py
+++ chat/middleware.py
@@ -6,7 +6,6 @@ from django.contrib.auth import get_user_model
 import jwt
 from django.conf import settings
 from urllib.parse import parse_qs
-import logging
 
 
 @database_sync_to_async
@@ -18,9 +17,6 @@ def get_user(user_id):
         return AnonymousUser()
 
 
-logger = logging.getLogger(__name__)
-
-
 class JWTAuthMiddleware(BaseMiddleware):
     async def __call__(self, scope, receive, send):
         query_params = parse_qs(scope['query_string'].decode())
@@ -30,7 +26,6 @@ class JWTAuthMiddleware(BaseMiddleware):
                 decoded_data = jwt.decode(token_key, settings.SECRET_KEY, algorithms=["HS256"])
                 scope['user'] = await get_user(decoded_data['id'])
             except (InvalidToken, TokenError, jwt.DecodeError, KeyError) as e:
-                logger.error(f"Token error: {e}")
                 scope['user'] = AnonymousUser()
         else:
             scope['user'] = AnonymousUser()
@@ -38,5 +33,4 @@ class JWTAuthMiddleware(BaseMiddleware):
         try:
             return await super().__call__(scope, receive, send)
         except Exception as e:
-            logger.error(f"WebSocket error: {e}")
             raise e
